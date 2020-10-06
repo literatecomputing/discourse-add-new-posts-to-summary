cbbf302d72d7cca72e140813bd80f9bda39fb6a8
Jay Pfaffman <pfaffman@gmail.com>
2020-10-06 13:09:28 -0700

    add specs

--- spec/requests/actions_controller_spec.rb
+++ spec/requests/actions_controller_spec.rb
@@ -1,13 +1,89 @@
 require 'rails_helper'
 
 describe DiscourseAddNewPostsToSummary::ActionsController do
+  let(:user) { Fabricate(:admin) }
+
   before do
     Jobs.run_immediately!
   end
 
-  it 'can list' do
-    sign_in(Fabricate(:user))
-    get "/discourse-add-new-posts-to-summary/list.json"
-    expect(response.status).to eq(200)
+  context "with new topics" do
+
+    subject { UserNotifications.digest(user) }
+
+    after do
+      Discourse.redis.keys('summary-new-users:*').each { |key| Discourse.redis.del(key) }
+    end
+
+    let!(:popular_topic) { Fabricate(:topic, user: Fabricate(:coding_horror), created_at: 1.hour.ago) }
+
+    # these are from discourse/spec/mailers/user_notifications_spec.rb
+    it "works" do
+      SiteSetting.discourse_add_new_posts_to_summary_enabled=true
+      SiteSetting.discourse_add_new_posts_include_new_post_count=true
+      expect(subject.to).to eq([user.email])
+      expect(subject.subject).to be_present
+      expect(subject.from).to eq([SiteSetting.notification_email])
+      expect(subject.html_part.body.to_s).to be_present
+      expect(subject.text_part.body.to_s).to be_present
+      expect(subject.header["List-Unsubscribe"].to_s).to match(/\/email\/unsubscribe\/\h{64}/)
+      expect(subject.html_part.body.to_s).to include('New Users')
+    end
+
+    it "supports subfolder" do
+      SiteSetting.discourse_add_new_posts_to_summary_enabled=true
+      SiteSetting.discourse_add_new_posts_include_new_post_count=true
+      set_subfolder "/forum"
+      html = subject.html_part.body.to_s
+      text = subject.text_part.body.to_s
+      expect(html).to be_present
+      expect(text).to be_present
+      expect(html).to_not include("/forum/forum")
+      expect(text).to_not include("/forum/forum")
+      expect(subject.header["List-Unsubscribe"].to_s).to match(/http:\/\/test.localhost\/forum\/email\/unsubscribe\/\h{64}/)
+
+      topic_url = "http://test.localhost/forum/t/#{popular_topic.slug}/#{popular_topic.id}"
+      expect(html).to include(topic_url)
+      expect(text).to include(topic_url)
+    end
+
+
+    # these are for the plugin
+    it "works if add-new-posts-to-summary is set" do
+      SiteSetting.discourse_add_new_posts_to_summary_enabled=true
+      expect(subject.html_part.body.to_s).to include('New Users')
+    end
+
+    it "includes New Posts" do
+      SiteSetting.discourse_add_new_posts_to_summary_enabled=true
+      SiteSetting.discourse_add_new_posts_include_new_post_count=true
+      expect(subject.html_part.body.to_s).to include('New Posts')
+    end
+
+    it "does not include New Posts if plugin not enabled " do
+      SiteSetting.discourse_add_new_posts_to_summary_enabled=false
+      SiteSetting.discourse_add_new_posts_include_new_post_count=true
+      expect(subject.html_part.body.to_s).to_not include('New Posts')
+    end
+
+    it "does not include New Posts if add to post setting not enabled" do
+      SiteSetting.discourse_add_new_posts_to_summary_enabled=true
+      SiteSetting.discourse_add_new_posts_include_new_post_count=false
+      expect(subject.html_part.body.to_s).to_not include('New Posts')
+    end
+
+    it "matches the original discourse html template" do
+      orig_text=File.read('./app/views/user_notifications/digest.text.erb')
+      plugin_text_all=(File.read('plugins/discourse-add-new-posts-to-summary/app/views/user_notifications/digest.text.erb'))
+      plugin_text_orig=plugin_text_all.lines[3..-1].join
+      expect((orig_text==plugin_text_orig))
+    end
+
+    it "matches the original discourse html template" do
+      orig_text=File.read('./app/views/user_notifications/digest.html.erb')
+      plugin_text_all=(File.read('plugins/discourse-add-new-posts-to-summary/app/views/user_notifications/digest.html.erb'))
+      plugin_text_orig=plugin_text_all.lines[3..-1].join
+      expect((orig_text==plugin_text_orig))
+    end
   end
 end
